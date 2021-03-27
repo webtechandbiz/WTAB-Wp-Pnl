ed857939912cd44b5cc111b77da6644b85a68532
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-03-27 15:34:18 +0100

    Update wtab_pnl_js.js

--- admin-pagibrebob/public_html/js/wtab_pnl_js.js
+++ admin-pagibrebob/public_html/js/wtab_pnl_js.js
@@ -10,5 +10,17 @@ jQuery( document ).ready(function() {
         }else{
             jQuery('#' + this_id).val('0');
         }
-    }); 
+    });
 });
+
+function wtab_upl(title, ext, label, attachto){
+    var _wtab_upl = wp.media({
+        title:title,
+        library:{type:ext},
+        button:{text:label},
+        multiple: false
+    }).on('select', function() {
+        var _uploadedfile = _wtab_upl.state().get('selection').first().toJSON();
+        jQuery('#' + attachto).val(_uploadedfile.url);
+    }).open();
+}
